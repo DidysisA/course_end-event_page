b438c63c68b814003cf4c3375bd57f8a363915ad
DidysisA <didysis.a@gmail.com>
2025-05-08 06:35:13 +0300

    usles fail

--- backend/models/Venue.js
+++
@@ -1,7 +0,0 @@
-const { Schema, model } = require('mongoose');
-const VenueSchema = new Schema({
-  name:    String,
-  address: String,
-  capacity:Number,
-}, { timestamps: true });
-module.exports = model('Venue', VenueSchema);
